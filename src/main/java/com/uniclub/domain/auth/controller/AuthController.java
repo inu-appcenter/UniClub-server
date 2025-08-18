0a7fe30c251762b09326b4e05792f4322061b9af
Jungseokhwan <jsh2910@naver.com>
2025-08-18 17:25:50 +0900

    Implement logging across all service layers

--- src/main/java/com/uniclub/domain/auth/controller/AuthController.java
+++ src/main/java/com/uniclub/domain/auth/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.uniclub.domain.auth.service.AuthService;
 import com.uniclub.global.swagger.AuthApiSpecification;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
